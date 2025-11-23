b7a4bbb37b5c746aafaedadb522188e59e808a88
Srini <srinismart2@gmail.com>
2025-11-23 12:53:51 +0530

    Sidebar updates

--- my-backstage-app/packages/app/src/components/Root/Root.tsx
+++ my-backstage-app/packages/app/src/components/Root/Root.tsx
@@ -2,6 +2,7 @@ import { PropsWithChildren } from 'react';
 import { makeStyles } from '@material-ui/core';
 import HomeIcon from '@material-ui/icons/Home';
 import ExtensionIcon from '@material-ui/icons/Extension';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
 import LibraryBooks from '@material-ui/icons/LibraryBooks';
 import CreateComponentIcon from '@material-ui/icons/AddCircleOutline';
 import LogoFull from './LogoFull';
@@ -76,6 +77,7 @@ export const Root = ({ children }: PropsWithChildren<{}>) => (
         <SidebarItem icon={ExtensionIcon} to="api-docs" text="APIs" />
         <SidebarItem icon={LibraryBooks} to="docs" text="Docs" />
         <SidebarItem icon={CreateComponentIcon} to="create" text="Create..." />
+        <SidebarItem icon={AddCircleIcon} to="catalog-import" text="Register an Existing Component" />
         {/* End global nav */}
         <SidebarDivider />
         <SidebarScrollWrapper>
